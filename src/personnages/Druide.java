2c65a62a18fac70899dc52536f15cf9a1662c12d
Wally-Meqqori <wally.meqqori@univ-tlse3.fr>
2024-09-18 15:09:24 +0200

    TP1

---
+++ src/personnages/Druide.java
@@ -0,0 +1,32 @@
+
+package personnages;
+import java.util.*;
+
+public class Druide {
+	private String nom;
+	private int effetPotionMin;
+	private int effetPotionMax;
+	private int forcePotion = 1;
+	
+	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à " + effetPotionMax + ".");
+	}
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+
+	public void preparerPotion() {
+		System.out.println("test");
+	}
+	private String prendreParole() {
+		return "Le druide " + nom + " : ";
+	}
+}
+
